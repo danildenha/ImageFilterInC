b9a572eaafa2f96823da5a39b4cb14faf79c55de
danildenha <danildenha@gmail.com>
2023-12-27 15:09:02 -0600

    structs

--- source/bmp.h
+++ source/bmp.h
@@ -7,9 +7,6 @@
  *
  * The data types in this section are essentially aliases for C/C++
  * primitive data types.
- *
- * Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
- * See http://en.wikipedia.org/wiki/Stdint.h for more on stdint.h.
  */
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
@@ -21,8 +18,6 @@ typedef uint16_t WORD;
  *
  * The BITMAPFILEHEADER structure contains information about the type, size,
  * and layout of a file that contains a DIB [device-independent bitmap].
- *
- * Adapted from http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
  */
 typedef struct
 {
@@ -33,4 +28,40 @@ typedef struct
     DWORD  bfOffBits;
 } __attribute__((__packed__))
 BITMAPFILEHEADER;
+
+/**
+ * BITMAPINFOHEADER
+ *
+ * The BITMAPINFOHEADER structure contains information about the
+ * dimensions and color format of a DIB [device-independent bitmap].
+ */
+typedef struct
+{
+    DWORD  biSize;
+    LONG   biWidth;
+    LONG   biHeight;
+    WORD   biPlanes;
+    WORD   biBitCount;
+    DWORD  biCompression;
+    DWORD  biSizeImage;
+    LONG   biXPelsPerMeter;
+    LONG   biYPelsPerMeter;
+    DWORD  biClrUsed;
+    DWORD  biClrImportant;
+} __attribute__((__packed__))
+BITMAPINFOHEADER;
+
+/**
+ * RGBTRIPLE
+ *
+ * This structure describes a color consisting of relative intensities of
+ * red, green, and blue.
+ */
+typedef struct
+{
+    BYTE  rgbtBlue;
+    BYTE  rgbtGreen;
+    BYTE  rgbtRed;
+} __attribute__((__packed__)) 
+RGBTRIPLE;
 
