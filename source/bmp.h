eab69944cc2b9eb0a75c6c48d44c18ab0b47cdf1
danildenha <danildenha@gmail.com>
2023-12-27 14:51:01 -0600

    creating needed sources

---
+++ source/bmp.h
@@ -0,0 +1,17 @@
+// BMP-related data types based on Microsoft's own
+
+#include <stdint.h>
+
+/**
+ * Common Data Types
+ *
+ * The data types in this section are essentially aliases for C/C++
+ * primitive data types.
+ *
+ * Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
+ * See http://en.wikipedia.org/wiki/Stdint.h for more on stdint.h.
+ */
+typedef uint8_t  BYTE;
+typedef uint32_t DWORD;
+typedef int32_t  LONG;
+typedef uint16_t WORD;
