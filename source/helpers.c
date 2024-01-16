38193c85e82f9d518bd86814ae17e16e7dd69cd8
danildenha <danildenha@gmail.com>
2024-01-16 17:11:05 -0600

    Update helpers.c

--- source/helpers.c
+++ source/helpers.c
@@ -116,7 +116,6 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             Gxr_total = Gxg_total = Gxb_total = Gyr_total = Gyg_total = Gyb_total = 0;
 
             for (int h = -1; h < 2; h++)
-            {
                 for (int l = -1; l < 2; l++)
                 {
         }
